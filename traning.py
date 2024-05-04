c3cad1aed73a82826e60a1f4519bdf91e71c9904
alireza kargar <alirezakargarsuport@gmail.com>
2024-05-04 14:57:13 +0330

    fix training bug

--- traning.py
+++ traning.py
@@ -41,17 +41,31 @@ pickle.dump(classes, open('classes.pkl', 'wb'))
 training = []
 output_empty = [0] * len(classes)
 
+print(words, 0)
 for document in documents:
+    print(document, 1) # 1
+
     bag = []
     word_patterns = document[0]
+    print(word_patterns, 2) # 2 
     word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
+
+    print(word_patterns, 3) # 3 
 
     for word in words:
         bag.append(1) if word in word_patterns else bag.append(0)
 
     output_row = list(output_empty)
+    print(bag, 4) # 4
+    print(output_row, 5) # 5
+    print("----------------------------------------")
     output_row[classes.index(document[1])] = 1
+    print(output_row, 6)
     training.append([bag, output_row])
+    print(training, 7)
+    print("<<<<<<<<<<<--------->>>>>>>>>>>>")
+
+# print(training)
 
 random.shuffle(training)
 """
@@ -62,8 +76,8 @@ This is a common preprocessing step before feeding the data into a machine learn
 
 training = np.array(training, dtype=object)
 
-train_x = list(training[:, 0])
-train_y = list(training[:, 1])
+train_x = list(training[:, 0]) # words
+train_y = list(training[:, 1]) # tags
 
 
 pickle.dump(train_x, open('train_x.pkl', 'wb'))
@@ -76,12 +90,14 @@ model.add(Dense(64, activation='relu'))
 model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]), activation='softmax'))
 
-# sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+# sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True) # not works
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 hist = model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+
 # Export the model to a HDF5 file
 # model.save('chatbotmodel.h5') # works
+
 model.save('chatbotmodel.keras')
 
 print("Done")
